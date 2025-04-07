7738d3b5057df7343c2bc29f18910e4286c2b06f
Gabriel-Anareta <gabrielallenanareta@gmail.com>
2025-04-06 22:00:17 -0600

    Add test for name

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders name on page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Gabriel Allen Anareta/i);
   expect(linkElement).toBeInTheDocument();
 });
